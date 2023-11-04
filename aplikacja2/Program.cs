21577c68479ff2126fa70cf933fcbcf0206fa5eb
hptss <szymonochojski@proton.me>
2023-11-04 10:48:47 +0100

    save1

---
+++ aplikacja2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace aplikacja2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Worl2d!");
+        }
+    }
+}
